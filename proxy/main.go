d69e2bfc0daf5e9c562a19684d0f8688baecada3
mahir jain <mahirjain.mj71@gmail.com>
2025-09-24 01:27:02 +0530

    added code to correctly serve directives of cache control , deleted the test folder

--- proxy/main.go
+++ proxy/main.go
@@ -17,8 +17,6 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-
-
 type CacheControl struct {
 	cacheType      string
 	maxAge         int // in seconds
@@ -26,7 +24,7 @@ type CacheControl struct {
 }
 
 type Cache struct {
-	CachType string 			   `json:"cache_type"`
+	CachType     string            `json:"cache_type"`
 	Body         string            `json:"body"`
 	Headers      map[string]string `json:"headers"`
 	StoredAt     int64             `json:"stored_at"`
@@ -76,7 +74,7 @@ func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if err == redis.Nil {
 		didCacheHit = false
 		fmt.Println("Cache miss, fetching from origin...")
-		fetchedData, err ,didWeHitCache:= p.fetchOnCacheMiss(ctx, key, r)
+		fetchedData, err, didWeHitCache := p.fetchOnCacheMiss(ctx, key, r)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadGateway)
 			return
@@ -84,7 +82,7 @@ func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		cachedData = *fetchedData
 		didCacheHit = didWeHitCache
 
-	} else if err != nil { 
+	} else if err != nil {
 		http.Error(w, "cache error: "+err.Error(), http.StatusInternalServerError)
 		return
 
@@ -98,18 +96,22 @@ func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 		isExpired := time.Now().Unix() > cachedData.StoredAt+int64(cachedData.MaxAge)
 		isUseStaleUptoExpired := time.Now().Unix() > cachedData.StoredAt+int64(cachedData.MaxAge)+int64(cachedData.UseStaleUpto)
-		
-		// If the max age (freshness) expired and STALE_WHILE_REVALIDATE 
+
+		fmt.Println("The cache type is : ", cachedData.CachType)
+		fmt.Println("Has cache expired : ", isExpired)
+		fmt.Println("Has stale expired : ", isUseStaleUptoExpired)
+
+		// If the max age (freshness) expired and STALE_WHILE_REVALIDATE
 		// Call the Origin Async
-		if isExpired && cachedData.CachType == constant.STALE_WHILE_REVALIDATE {
+		if isExpired && !isUseStaleUptoExpired && cachedData.CachType == constant.STALE_WHILE_REVALIDATE {
 			p.revalidateAsynchronously(key, &cachedData, r)
 		}
 
 		// If cache expired && MUST_REVALIDATE
 		// If isUseStaleUptoExpired (max age + stale upto) && STALE_IF_ERROR
 		// If isUseStaleUptoExpired (max age + stale upto) && STALE_WHILE_REVALIDATE
-		// Call the Origin Sync 
-		if isExpired && cachedData.CachType == constant.MUST_REVALIDATE || isUseStaleUptoExpired && cachedData.CachType == constant.STALE_IF_ERROR || isUseStaleUptoExpired && cachedData.CachType == constant.STALE_WHILE_REVALIDATE{
+		// Call the Origin Sync
+		if isExpired && cachedData.CachType == constant.MUST_REVALIDATE || isUseStaleUptoExpired && cachedData.CachType == constant.STALE_IF_ERROR || isUseStaleUptoExpired && cachedData.CachType == constant.STALE_WHILE_REVALIDATE {
 			fmt.Println("Cache is expired and must be revalidated synchronously.")
 			revalidatedData, err := p.revalidateSynchronously(ctx, key, &cachedData, r)
 			if err != nil {
@@ -120,7 +122,6 @@ func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	
 	for k, v := range cachedData.Headers {
 		w.Header().Set(k, v)
 	}
@@ -138,12 +139,11 @@ func (p *Proxy) getHealthyOriginPort() string {
 	return ""
 }
 
-
-func (p *Proxy) fetchOnCacheMiss(ctx context.Context, key string, r *http.Request) (*Cache, error,bool) {
+func (p *Proxy) fetchOnCacheMiss(ctx context.Context, key string, r *http.Request) (*Cache, error, bool) {
 	myChan := make(chan struct{})
 	actualChan, loaded := p.cacheManager.leaderMap.LoadOrStore(key, myChan)
-	fmt.Println("Is did we load in cache miss : ",loaded)
-	fmt.Println("If no then channel in cache miss : ",actualChan)
+	fmt.Println("Is did we load in cache miss : ", loaded)
+	fmt.Println("If no then channel in cache miss : ", actualChan)
 
 	if !loaded { // I am the leader
 		fmt.Println("I am the leader for a cache miss.")
@@ -151,22 +151,22 @@ func (p *Proxy) fetchOnCacheMiss(ctx context.Context, key string, r *http.Reques
 			close(actualChan.(chan struct{}))
 			p.cacheManager.leaderMap.Delete(key)
 		}()
-		
+
 		// Check for healthy port
 		portToUse := p.getHealthyOriginPort()
-		
-		// If no healthy port found ,send error 
+
+		// If no healthy port found ,send error
 		if portToUse == "" {
-			return nil, fmt.Errorf("no healthy origin server available"),false
+			return nil, fmt.Errorf("no healthy origin server available"), false
 		}
 		fullOrigin := p.origin + portToUse
 
-				time.Sleep(10 * time.Second)
+		time.Sleep(10 * time.Second)
 		fmt.Println("10 seconds finished...")
 		// Fetch from Origin
-		body, _, headers, fetchErr := fetchFromOrigin(fullOrigin, "", r)
+		body, _, headers, fetchErr := fetchFromOrigin(ctx, fullOrigin, "", r)
 		if fetchErr != nil {
-			return nil, fetchErr,false
+			return nil, fetchErr, false
 		}
 
 		// Get the value of cache-control header to parse
@@ -177,7 +177,7 @@ func (p *Proxy) fetchOnCacheMiss(ctx context.Context, key string, r *http.Reques
 
 		// Ready the data to save into redis
 		cacheToStore := &Cache{
-			CachType: 	cacheControlStruct.cacheType,
+			CachType:     cacheControlStruct.cacheType,
 			Body:         body,
 			Headers:      headers,
 			StoredAt:     time.Now().Unix(),
@@ -189,10 +189,10 @@ func (p *Proxy) fetchOnCacheMiss(ctx context.Context, key string, r *http.Reques
 		if marshalErr != nil {
 			log.Printf("Error marshaling cache data: %v", marshalErr)
 		} else {
-			fmt.Println("Saving data in Redis for cache MISS : ",loaded)
+			fmt.Println("Saving data in Redis for cache MISS : ", loaded)
 			p.cacheManager.redisClient.Set(ctx, key, cacheJSON, 0)
 		}
-		return cacheToStore, nil,true
+		return cacheToStore, nil, true
 
 	} else { // I am a follower
 		fmt.Println("Another request is already fetching for this cache miss. I will wait.")
@@ -201,19 +201,18 @@ func (p *Proxy) fetchOnCacheMiss(ctx context.Context, key string, r *http.Reques
 			fmt.Println("Follower unblocked! Re-fetching from cache after miss.")
 			value, err := p.cacheManager.redisClient.Get(ctx, key).Result()
 			if err != nil {
-				return nil, fmt.Errorf("failed to retrieve cache after leader's fetch"),false
+				return nil, fmt.Errorf("failed to retrieve cache after leader's fetch"), false
 			}
 			var cachedData Cache
 			if unmarshalErr := json.Unmarshal([]byte(value), &cachedData); unmarshalErr != nil {
-				return nil, fmt.Errorf("failed to unmarshal cache for follower"),false
+				return nil, fmt.Errorf("failed to unmarshal cache for follower"), false
 			}
-			return &cachedData, nil,false
+			return &cachedData, nil, false
 		case <-ctx.Done():
-			return nil, fmt.Errorf("request timed out while waiting for initial cache fill"),false
+			return nil, fmt.Errorf("request timed out while waiting for initial cache fill"), false
 		}
 	}
 }
-
 
 func (p *Proxy) revalidateSynchronously(ctx context.Context, key string, currentData *Cache, r *http.Request) (*Cache, error) {
 	portToUse := p.getHealthyOriginPort()
@@ -224,25 +223,22 @@ func (p *Proxy) revalidateSynchronously(ctx context.Context, key string, current
 	myChan := make(chan struct{})
 	actualChan, loaded := p.cacheManager.leaderMap.LoadOrStore(key, myChan)
 
+	fmt.Println("Is did we load in cache miss : ", loaded)
+	fmt.Println("If no then channel in cache miss : ", actualChan)
+
 	if !loaded { // I am the leader.
 		fmt.Println("Cache expired. I am the leader, revalidating...")
-		defer func() {
-			close(actualChan.(chan struct{}))
-			p.cacheManager.leaderMap.Delete(key)
-			fmt.Println("Leader finished revalidation and cleaned up.")
-		}()
-
-		
-		bgCtx, bgCancel := context.WithTimeout(context.Background(), 25*time.Second)
-		defer bgCancel()
 		time.Sleep(10 * time.Second) // Simulating work
-		if err := revalidateAndUpdateCache(bgCtx, p.cacheManager.redisClient, key, currentData, p.origin, portToUse, r); err != nil {
+		if err := revalidateAndUpdateCache(ctx, p.cacheManager.redisClient, key, currentData, p.origin, portToUse, r); err != nil {
 			log.Printf("Error during cache revalidation: %v", err)
 			return nil, err
 		}
+		fmt.Println("Closing Channel.")
+		close(actualChan.(chan struct{}))
+		p.cacheManager.leaderMap.Delete(key)
+		fmt.Println("Leader finished revalidation and cleaned up.")
 	}
-
-	fmt.Println("Waiting for leader to finish revalidation...")
+	fmt.Println("Waiting for leader to finish revalidation sync...")
 	select {
 	case <-actualChan.(chan struct{}):
 		fmt.Println("Unblocked! Re-fetching updated item from cache.")
@@ -261,7 +257,6 @@ func (p *Proxy) revalidateSynchronously(ctx context.Context, key string, current
 	}
 }
 
-
 func (p *Proxy) revalidateAsynchronously(key string, currentData *Cache, r *http.Request) {
 	portToUse := p.getHealthyOriginPort()
 	if portToUse == "" {
@@ -275,25 +270,22 @@ func (p *Proxy) revalidateAsynchronously(key string, currentData *Cache, r *http
 	if !loaded { // I am the leader.
 		fmt.Println("Cache stale. I am the leader, revalidating in the background...")
 		go func() {
-			defer func() {
-				
-				p.cacheManager.leaderMap.Delete(key)
-				fmt.Println("Async leader finished revalidation and cleaned up.")
-			}()
+			bgCtx, cancel := context.WithTimeout(context.Background(), 25*time.Second)
+			defer cancel()
 
-			bgCtx, bgCancel := context.WithTimeout(context.Background(), 25*time.Second)
-			defer bgCancel()
-			time.Sleep(10 * time.Second) 
+			time.Sleep(10 * time.Second)
 
 			dataCopy := *currentData
 			if err := revalidateAndUpdateCache(bgCtx, p.cacheManager.redisClient, key, &dataCopy, p.origin, portToUse, r); err != nil {
 				log.Printf("Error during async cache revalidation: %v", err)
 			}
+			p.cacheManager.leaderMap.Delete(key)
+			fmt.Println("Async leader finished revalidation and cleaned up.")
 		}()
+	} else {
+		fmt.Println("I am a follower and I am waiting")
 	}
-	
 }
-
 
 func initializeRedisClient(redisURL string) (*redis.Client, error) {
 	opt, err := redis.ParseURL(redisURL)
@@ -316,24 +308,24 @@ func (cache *CacheControl) getCacheControlDataFromHeader(headerValue string) {
 	for _, part := range parts {
 		trimmedPart := strings.TrimSpace(part)
 		isStaleWhileRevalidateDir := strings.HasPrefix(trimmedPart, "stale-while-revalidate=")
-		staleIfErrorDir := strings.HasPrefix(trimmedPart,"stale-if-error=")
+		staleIfErrorDir := strings.HasPrefix(trimmedPart, "stale-if-error=")
 		if strings.Contains(trimmedPart, constant.MUST_REVALIDATE) {
 			// Save Must Revalidate
 			cache.cacheType = constant.MUST_REVALIDATE
-		} else if strings.HasPrefix(trimmedPart, "max-age=") { 
+		} else if strings.HasPrefix(trimmedPart, "max-age=") {
 			// Save Max Age
 			value, err := strconv.Atoi(strings.Split(trimmedPart, "=")[1])
 			if err == nil {
 				cache.maxAge = value
 			}
-		} else if  staleIfErrorDir || isStaleWhileRevalidateDir { 
+		} else if staleIfErrorDir || isStaleWhileRevalidateDir {
 			value, err := strconv.Atoi(strings.Split(trimmedPart, "=")[1])
 			if err == nil {
-				if (isStaleWhileRevalidateDir) {
+				if isStaleWhileRevalidateDir {
 					cache.cacheType = constant.STALE_WHILE_REVALIDATE
-				}else{
+				} else {
 					cache.cacheType = constant.STALE_IF_ERROR
-					
+
 				}
 				cache.serveStaleUpto = value
 			}
@@ -341,13 +333,13 @@ func (cache *CacheControl) getCacheControlDataFromHeader(headerValue string) {
 	}
 }
 
-func fetchFromOrigin(origin string, etag string, r *http.Request) (string, int, map[string]string, error) {
+func fetchFromOrigin(ctx context.Context, origin string, etag string, r *http.Request) (string, int, map[string]string, error) {
 	url := origin + r.URL.Path
 	if r.URL.RawQuery != "" {
 		url += "?" + r.URL.RawQuery
 	}
 
-	req, err := http.NewRequestWithContext(r.Context(), http.MethodGet, url, nil)
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
 		return "", http.StatusInternalServerError, nil, fmt.Errorf("error creating request: %w", err)
 	}
@@ -383,7 +375,7 @@ func fetchFromOrigin(origin string, etag string, r *http.Request) (string, int, 
 func revalidateAndUpdateCache(ctx context.Context, redisClient *redis.Client, key string, cachedData *Cache, origin, portToUse string, r *http.Request) error {
 	fullOrigin := origin + portToUse
 	etag := cachedData.Headers["Etag"]
-	newBody, statusCode, newHeaders, err := fetchFromOrigin(fullOrigin, etag, r)
+	newBody, statusCode, newHeaders, err := fetchFromOrigin(ctx, fullOrigin, etag, r)
 	if err != nil {
 		return fmt.Errorf("failed to revalidate from origin: %w", err)
 	}
@@ -424,7 +416,6 @@ func CheckHealth(ports []string, origin string, healthCheckMap map[string]bool) 
 	}
 }
 
-
 func main() {
 	originPorts := flag.String("ports", "8000", "Comma-separated list of origin server ports")
 	origin := flag.String("origin", "http://localhost:", "Default Origin server URL")
@@ -455,11 +446,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	
 	cacheManager := NewCacheManager(redisClient)
 	proxyHandler := NewProxy(*origin, healthCheckMap, cacheManager)
 
-	
 	http.Handle("/", proxyHandler)
 
 	fmt.Println("Proxy server started successfully at :3000")
